0a96db23619b6a1e8c6c49a73ae183f72ba7c3c9
Ryan Crichton <rg.crichton@gmail.com>
2016-02-18 16:06:16 +0200

    Improve mediator import and configuration for datim
    The mediator config for DATIM is now stored in this repository so that
    it is easy to update the mediators when a new version is out. The copy
    mediators script now downloads the mediators from git and applies the
    config as specified in this repo.

--- cp-mediators-into-pkg.sh
+++ cp-mediators-into-pkg.sh
@@ -4,7 +4,22 @@ set -e
 SRCDIR="/home/ryan/git"
 
 mkdir -p targets/trusty/usr/share
+rm -rf targets/trusty/usr/share/*
 
-cp -r $SRCDIR/openhim-mediator-datim targets/trusty/usr/share
-cp -r $SRCDIR/openhim-mediator-file-queue targets/trusty/usr/share
-cp -r $SRCDIR/openhim-mediator-adx-csd-mapper targets/trusty/usr/share
+echo "Cloning base mediators..."
+git clone https://github.com/jembi/openhim-mediator-datim.git targets/trusty/usr/share/openhim-mediator-datim
+git clone https://github.com/jembi/openhim-mediator-file-queue.git targets/trusty/usr/share/openhim-mediator-file-queue
+git clone https://github.com/jembi/openhim-mediator-adx-csd-mapper.git targets/trusty/usr/share/openhim-mediator-adx-csd-mapper
+echo "Done."
+
+echo "Configuring mediators..."
+cp -r mediator-config/openhim-mediator-datim/config targets/trusty/usr/share/openhim-mediator-datim/
+cp -r mediator-config/openhim-mediator-file-queue/config targets/trusty/usr/share/openhim-mediator-file-queue/
+cp -r mediator-config/openhim-mediator-adx-csd-mapper/config targets/trusty/usr/share/openhim-mediator-adx-csd-mapper/
+echo "Done."
+
+echo "Downloading module dependencies..."
+(cd targets/trusty/usr/share/openhim-mediator-datim/ && npm install)
+(cd targets/trusty/usr/share/openhim-mediator-file-queue/ && npm install)
+(cd targets/trusty/usr/share/openhim-mediator-adx-csd-mapper/ && npm install)
+echo "Done."
