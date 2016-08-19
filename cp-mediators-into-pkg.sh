dab0af0c373e7676b401bbc58dc7ba780c1efe13
Ryan Crichton <ryan@jembi.org>
2016-08-19 10:45:22 +0200

    Add intial sync mediator config

--- cp-mediators-into-pkg.sh
+++ cp-mediators-into-pkg.sh
@@ -7,14 +7,17 @@ rm -rf targets/trusty/usr/share/*
 echo "Cloning base mediators..."
 git clone https://github.com/jembi/openhim-mediator-datim.git targets/trusty/usr/share/openhim-mediator-datim
 git clone https://github.com/jembi/openhim-mediator-file-queue.git targets/trusty/usr/share/openhim-mediator-file-queue
+git clone https://github.com/jembi/openhim-mediator-openinfoman-dhis2-sync.git targets/trusty/usr/share/openhim-mediator-openinfoman-dhis2-sync
 echo "Done."
 
 echo "Configuring mediators..."
 cp -r mediator-config/openhim-mediator-datim/config targets/trusty/usr/share/openhim-mediator-datim/
 cp -r mediator-config/openhim-mediator-file-queue/config targets/trusty/usr/share/openhim-mediator-file-queue/
+cp -r mediator-config/openhim-mediator-openinfoman-dhis2-sync/config targets/trusty/usr/share/openhim-mediator-openinfoman-dhis2-sync/
 echo "Done."
 
 echo "Downloading module dependencies..."
 (cd targets/trusty/usr/share/openhim-mediator-datim/ && npm install)
 (cd targets/trusty/usr/share/openhim-mediator-file-queue/ && npm install)
+(cd targets/trusty/usr/share/openhim-mediator-openinfoman-dhis2-sync/ && npm install)
 echo "Done."
