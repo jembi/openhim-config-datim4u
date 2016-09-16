2f720d6e67cab650657194e862f067bebf0f5727
Ryan Crichton <ryan@jembi.org>
2016-09-16 08:41:24 +0200

    Fix assign datasets form

--- targets/trusty/opt/openhim-shell-scripts/form-assignment.sh
+++ targets/trusty/opt/openhim-shell-scripts/form-assignment.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-curl -X POST -u $DHIS_USER:$DHIS_PASS http://localhost:8084/dhis2/api/resourceTables
-echo "Sleeping for 2 mins to let resource rebuild task complete"
-sleep 120
 sudo -S -u postgres psql -f /usr/share/dhis2-datim/all_dataset_assignments_and_orgunit_group_memberships.sql dhis2
 sleep 2
 curl -s -o /dev/null -X PUT -u $DHIS_USER:$DHIS_PASS http://localhost:8084/dhis2/api/maintenance/cacheClear
